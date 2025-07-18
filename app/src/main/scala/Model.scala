c98a705dc296f79e23c077b852703fe0109d7345
tmKla <tm.klajn@gmail.com>
2025-07-18 17:17:32 +0200

    builing graph done

--- app/src/main/scala/Model.scala
+++ app/src/main/scala/Model.scala
@@ -2,11 +2,7 @@
 package efrei.app
 
 // Utiliser un `enum` est plus propre pour des valeurs fixes comme les noms de campus
-enum Campus(val nom: String) {
-  case Gorki extends Campus("Gorki")
-  case Republique extends Campus("République")
-  case Home extends Campus("Home")
-}
+case class Building(name : String)
 
 // Le sommet de votre graphe orienté sera une salle, identifiée par son nom et son campus
-case class Salle(nom: String, campus: Campus)
+case class Salle(nom: String, building: Building)
